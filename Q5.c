4e339d9249ad873ffe3273c5c353a4682c4e3368
Victor Rocha <Victor.rocha2002@gmail.com>
2022-08-03 21:41:25 -0300

    alteraçao ex5

--- Q5.c
+++ Q5.c
@@ -4,16 +4,12 @@
 int main(){
     char string[6], novaString[6];
     int i, contador=0;
+
+    print("Digite a string: \n");
     scanf(" %s", string);
-    for(i=0; i<6; i++){
-        printf("%c", string[i]);
-    }
 
     for(i=6; i>0; i--){
         novaString[contador]=string[i];
-        printf("\nletra: %c", novaString[contador]);
-        printf("\ncontador:%d", contador);
-        printf("\ni:%d", i);
         contador++;
     }
     for(i=0; i<6; i++){
